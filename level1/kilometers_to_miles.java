c5eea4d1e65724a99651697e6953556638896faf
AYUSH PRATAP SINGH <PRATAP.AYUSH0211@GMAIL.COM>
2025-08-20 23:38:11 +0530

    Added level1 folder

---
+++ level1/kilometers_to_miles.java
@@ -0,0 +1,10 @@
+public class kilometers_to_miles {
+   public kilometers_to_miles() {
+   }
+
+   public static void main(String[] var0) {
+      double var1 = 10.8;
+      double var3 = 1.6 * var1;
+      System.out.println("The distance " + var1 + "km in miles is " + var3);
+   }
+}
