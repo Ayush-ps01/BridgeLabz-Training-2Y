881f9db3306c988682ad49e4b061fb6eaf4c6beb
AYUSH PRATAP SINGH <PRATAP.AYUSH0211@GMAIL.COM>
2025-11-06 15:10:17 +0530

    Delete Object Oriented Design Principles Practices directory

--- Object Oriented Design Principles Practices/Submission of Class, Object and Sequenece Diagram/school_results/Subject.java
+++
@@ -1,9 +0,0 @@
-public class Subject {
-    private String name;
-    private double marks;
-    public Subject(String name, double marks) {
-        this.name = name; this.marks = marks;
-    }
-    public double getMarks() { return marks; }
-    public String getName() { return name; }
-}
